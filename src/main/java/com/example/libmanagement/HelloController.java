f006e015f052c5c6be795973b3ecf124952b2f79
Sakith Nimpura <sakith1256@gmail.com>
2023-05-02 21:05:50 +0530

    ok

--- src/main/java/com/example/libmanagement/HelloController.java
+++ src/main/java/com/example/libmanagement/HelloController.java
@@ -5,21 +5,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloController {
 
@@ -35,7 +32,7 @@ public class HelloController {
         Stage stage = (Stage) shop.getScene().getWindow();
         stage.close();
         Stage primaryStage = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("subject.fxml"));
         primaryStage.setTitle("IIt library management system login");
         primaryStage.setScene(new Scene (root));
         primaryStage.show();
@@ -45,8 +42,69 @@ public class HelloController {
     void slide1(MouseEvent event) {
 
     }
+    @FXML
+    private ImageView libr1;
 
     @FXML
+    private ImageView student1;
+
+    @FXML
+    void libriyan(MouseEvent event) throws IOException {
+        Stage stage = (Stage) libr1.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("LOGINlIB.fxml"));
+        primaryStage.setTitle("IIt library management system login");
+        primaryStage.setScene(new Scene (root));
+        primaryStage.show();
+    }
+
+    @FXML
+    void student(MouseEvent event) throws IOException {
+        Stage stage = (Stage)student1.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("IIt library management system login");
+        primaryStage.setScene(new Scene (root));
+        primaryStage.show();
+    }
+
+    @FXML
+    private TextField Uname1;
+
+    @FXML
+    private Button login1;
+
+    @FXML
+    private PasswordField passWord1;
+
+    @FXML
+    private Button sign12;
+
+    @FXML
+    private Label text2;
+
+    @FXML
+    void login8(ActionEvent event) throws IOException {
+        Stage stage = (Stage) login.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("subject.fxml"));
+        primaryStage.setTitle("IIt library management system sign in");
+        primaryStage.setScene(new Scene (root));
+        primaryStage.show();
+    }
+
+    @FXML
+    void sign8(ActionEvent event) {
+        Alert alert1 = new Alert ( Alert.AlertType.CONFIRMATION );
+        alert1.setContentText ( "Already send a massage to  IIT IT mananagemant" );
+        Optional<ButtonType> result1 = alert1.showAndWait ();
+        ButtonType button = result1.orElse ( ButtonType.CANCEL );
+    }
+
+        @FXML
     private TextField Uname;
 
     @FXML
@@ -129,38 +187,6 @@ public class HelloController {
         primaryStage.setTitle("Chowbay login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-    }
-    @FXML
-    private Button bs;
-
-    @FXML
-    private Button cool;
-
-    @FXML
-    private Button cs;
-
-    @FXML
-    private Button cs1;
-
-    @FXML
-    private Button cs2;
-
-    @FXML
-    private Button cs3;
-
-    @FXML
-    void bake(ActionEvent event) {
-
-    }
-
-    @FXML
-    void cool1(ActionEvent event) {
-
-    }
-
-    @FXML
-    void cs1(ActionEvent event) {
-
     }
 
 
